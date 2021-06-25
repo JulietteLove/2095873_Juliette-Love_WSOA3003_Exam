b97a520dc88a28441b1c42b5e4050263bef1b66b
JulietteLove <80416448+JulietteLove@users.noreply.github.com>
2021-06-25 22:29:30 +0200

    Submission

---
+++ 2095873 (Juliette Love) Exam Folder/Assets/Scripts/RotatingPlatform.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingPlatform : MonoBehaviour
+{
+    public GameObject rotatingPlatform;
+    public GameObject MouseOverHighlight;
+
+    private void OnMouseOver()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            rotatingPlatform.transform.Rotate(Vector3.forward, 90.0f);
+
+            InstructionScript instructionScript = GameObject.FindWithTag("Button").GetComponent<InstructionScript>();
+            instructionScript.Hitbox3.SetActive(false);
+
+            RatMovement ratMovement = GameObject.FindWithTag("Rat").GetComponent<RatMovement>();
+            ratMovement.maxSpeed = 3f;
+        }
+
+        MouseOverHighlight.SetActive(true);
+    }
+
+    private void OnMouseExit()
+    {
+        MouseOverHighlight.SetActive(false);
+    }
+}
---
+++ 2095873 (Juliette Love) Exam Folder/Assets/Scripts/Vents.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Vents : MonoBehaviour
+{
+    public bool ventIsOn = true;
+    public GameObject MouseOverHighlight;
+    public GameObject ParticleSystem;
+
+    public bool canClick = true;
+    public bool canActivate = true;
+
+    void Update()
+    {
+        if (ventIsOn == true)
+        {
+            GetComponent<AreaEffector2D>().enabled = true;
+            ParticleSystem.SetActive(true);
+        }
+        else if (ventIsOn == false)
+        {
+            GetComponent<AreaEffector2D>().enabled = false;
+            ParticleSystem.SetActive(false);
+        }
+    }
+
+    private void OnMouseOver()
+    {            
+        RatMovement ratMovement = GameObject.FindWithTag("Rat").GetComponent<RatMovement>();
+
+        if (Input.GetKeyDown(KeyCode.Mouse0) && canClick == true)
+        {
+            if (ventIsOn == false)
+            {
+                ventIsOn = true;
+            }
+            else if (ventIsOn == true)
+            {
+                ventIsOn = false;
+            }
+
+            ratMovement.maxSpeed = 3f;
+        }
+
+        MouseOverHighlight.SetActive(true);
+    }
+
+    private void OnMouseExit()
+    {
+        MouseOverHighlight.SetActive(false);
+    }
+}
